b459b29015f0b70925ed62c62b05680257c70469
Adhamovman <adxamovmuxammadsodiq@gmail.com>
2023-04-09 14:18:53 +0500

    fir

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const levelSection = document.getElementById('level');
+const levelSection = document.getElementById("level");
 const buttons = document.querySelectorAll(".level-btn");
-
+const gameSection = document.getElementById("game");
 buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        let level = e.target.dataset.id;
-        levelSection.classList.add("animation");
-        setTimeout(() => {
-            levelSection.classList.add("d-none");
-        }, 1000);
-    });
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let level = e.target.dataset.id;
+
+    localStorage.setItem("level", level);
+
+    levelSection.classList.add("levelAnimation");
+    setTimeout(() => {
+      levelSection.classList.add("d-none");
+    }, 1000);
+    gameSection.classList.remove("d-none");
+    gameSection.classList.add("gameAnimation");
+  });
 });
