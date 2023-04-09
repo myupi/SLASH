3bd1880ec7cba1eed4373d00adc004982a08f84d
Adhamovman <adxamovmuxammadsodiq@gmail.com>
2023-04-09 15:59:57 +0500

    fir

---
+++ js/const.js
@@ -0,0 +1,38 @@
+const easyLvl = [
+    { id: 1, english: "Mother", uzbek: "Ona" },
+    { id: 2, english: "Head", uzbek: "Bosh" },
+    { id: 3, english: "Hair", uzbek: "Soch" },
+    { id: 4, english: "Dog", uzbek: "It" },
+    { id: 5, english: "Fish", uzbek: "Baliq" },
+    { id: 6, english: "Cow", uzbek: "Sigir" },
+    { id: 7, english: "Red", uzbek: "Qizil" },
+    { id: 8, english: "Water", uzbek: "Suv" },
+    { id: 9, english: "Apple", uzbek: "Olma" },
+    { id: 10, english: "Window", uzbek: "Oyna" },
+  ];
+  
+  const mediumLvl = [
+    { id: 1, english: "Clothes", uzbek: "Kiyimlar" },
+    { id: 2, english: "Example", uzbek: "Misol" },
+    { id: 3, english: "Island", uzbek: "Orol" },
+    { id: 4, english: "Science", uzbek: "Fan" },
+    { id: 5, english: "Kind", uzbek: "Mehribon" },
+    { id: 6, english: "Great", uzbek: "Ajoyib" },
+    { id: 7, english: "Disease", uzbek: "Kasallik" },
+    { id: 8, english: "Climb", uzbek: "Ko'tarilish" },
+    { id: 9, english: "Paper", uzbek: "Qog'oz" },
+    { id: 10, english: "Letter", uzbek: "Xat" },
+  ];
+  
+  const hardLvl = [
+    { id: 1, english: "Abasement", uzbek: "Xo'rlash" },
+    { id: 2, english: "Covert", uzbek: "Yashirin" },
+    { id: 3, english: "Hamper", uzbek: "To'siq" },
+    { id: 4, english: "Laconic", uzbek: "Qisqa" },
+    { id: 5, english: "Oblivious", uzbek: "E'tiborsiz" },
+    { id: 6, english: "Great", uzbek: "Ajoyib" },
+    { id: 7, english: "Abroad", uzbek: "Chet elda" },
+    { id: 8, english: "Climb", uzbek: "Ko'tarilish" },
+    { id: 9, english: "Ancestor", uzbek: "Ajdod" },
+    { id: 10, english: "Await", uzbek: "Kutish" },
+  ];
